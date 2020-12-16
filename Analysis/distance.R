e59a2ffa6e068d053f5e938f3678932d150302ed
tlfzgjadyd <tlfzgjadyd@naver.com>
2020-12-16 23:05:09 +0900

    바꾸는도중 세이브

--- Analysis/distance.R
+++ Analysis/distance.R
@@ -1,10 +1,19 @@
 library(httr)
 library(XML)
-start<-"therivercafe"
-end<-"도서관"
+url_places<-"http://127.0.0.1:5500/html/cost.html"
+web_places<-GET(url_places)
+html_places<-htmlTreeParse(web_places, useInternalNodes=T, trim=T, encoding='utf-8')
+rootNode_places<-xmlRoot(html_places)
+start<-xpathSApply(rootNode_places, "//body/div/div[@class='main']/div[1]/div[1]/input[1]", xmlAttrs)
+start<- str_trim(start)
+start<-start[4]
+start<-str_replace_all(start,' ','+')
+end<-xpathSApply(rootNode_placeslaces, "//body/div/div[@class='main']/div[1]/div[1]/input[2]", xmlAttrs)
+end<- str_trim(end)
+end<-end[4]
+end<-str_replace_all(end, ' ','+')
 url<-paste("https://google.co.kr/search?q=",start,"%28뉴욕%29에서+",end,"%28뉴욕%29까지의+거리", sep="")
 web<-GET(url)
-help(paste)
 html<-htmlTreeParse(web, useInternalNodes=T, trim=T, encoding='utf-8')
 rootNode<-xmlRoot(html)
 tag<-xpathSApply(rootNode, "//body/div[@id='main']/div[3]", xmlValue)
@@ -12,6 +21,8 @@ library(stringr)
 mile <- str_trim(substr(tag, regexpr('\\(', tag)+1, regexpr('마일',tag)-1))
 miles<-as.numeric(mile)
 km<-miles*1.60934
-km<-round(km, digit=2)
+km<-round(km, digit=1)
 setwd("./Analysis")
 write(km, file="distance.txt")
+km
+
--- js/cost.js
+++ js/cost.js
@@ -13,6 +13,81 @@ function readTextFile(file) {
     };
     rawFile.send(null);
 }
+function initMap() {
+    const bounds = new google.maps.LatLngBounds();
+    const markersArray = [];
+    const origin1 = { lat: 55.93, lng: -3.18 };
+    const origin2 = "Greenwich,Park Row, England";
+    const destinationA = "Sweden";
+    const destinationB = { lat: 50.087, lng: 14.421 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 40.6, lng: -73.9 },
+      zoom: 10,
+    });
+    const geocoder = new google.maps.Geocoder();
+    const service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+      {
+        origins: [origin1, origin2],
+        destinations: [destinationA,destinationB],
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC,
+        avoidHighways: false,
+        avoidTolls: false,
+      },
+      (response, status) => {
+        if (status !== "OK") {
+          alert("Error was: " + status);
+        } else {
+         console.log(response)
+          const originList = response.originAddresses;
+          const destinationList = response.destinationAddresses;
+          const outputDiv = document.getElementById("output");
+          outputDiv.innerHTML = "";
+          deleteMarkers(markersArray);
+  
+          const showGeocodedAddressOnMap = function (asDestination) {
+
+  
+            return function (results, status) {
+              if (status === "OK") {
+                map.fitBounds(bounds.extend(results[0].geometry.location));
+
+              } else {
+                alert("Geocode was not successful due to: " + status);
+              }
+            };
+          };
+  
+          for (let i = 0; i < originList.length; i++) {
+            const results = response.rows[i].elements;
+            geocoder.geocode(
+              { address: originList[i] },
+              showGeocodedAddressOnMap(false)
+            );
+  
+            for (let j = 0; j < results.length; j++) {
+              geocoder.geocode(
+                { address: destinationList[j] },
+                showGeocodedAddressOnMap(true)
+              );
+              outputDiv.innerHTML +=
+                parseFloat(results[j].distance.text).toFixed(2);
+              let dist=parseFloat(results[j].distance.text).toFixed(2);
+              $('#receipt_text4').attr('data-flag', dist)
+            }
+          }
+        }
+      }
+    );
+  }
+  
+  function deleteMarkers(markersArray) {
+    for (let i = 0; i < markersArray.length; i++) {
+      markersArray[i].setMap(null);
+    }
+    markersArray = [];
+  }
 $(document).ready(function(){
     var select_num=2;
     $('.check_group').click(function(e){
@@ -23,17 +98,19 @@ $(document).ready(function(){
         $(img_str).attr('src', '../images/check_after.png');
     })
     $('#play_btn').click(function(){
-        let starting_point=$('#start_place_text').attr('value');
-        let destination=$('#end_place_text').attr('value');
+        $('#receipt_text4').text($('#receipt_text4').attr('data-flag'))
+        let starting_point=$('#start_place_text').val();
+        let destination=$('#end_place_text').val();
         let time_zone=$('#check_label'+select_num).attr('value');
-        
         $('#receipt_text1').text(starting_point);
         $('#receipt_text2').text(destination);
         $('#receipt_text3').text(time_zone);
+        
         //let str="https://www.google.co.kr/maps/dir/"+starting_point+", New York/"+destination+", New York";
         //window.open(str);
         
-        readTextFile("../Analysis/distance.txt");
-        //영수증 업데이트
+        //readTextFile("../Analysis/distance.txt");
     })
+
+
 })
